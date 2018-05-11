5a7c01ee153864bf2f502ffe176396b7cd600a19
HSelenat <942430793@qq.com>
2018-05-11 22:15:23 +0800

    通过侧边栏搜索框输入地址，地图上显示相应的位置和动画加载效果

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # frontend-nanodegree-map
 街区地图项目
+GitHub地址( https://github.com/hselenat/frontend-nanodegree-map)
+
+## 项目简介
+该项目是使用google地图API 和 纽约时报 API,通过异步加载获取地图和相关的数据。通过侧边栏搜索框输入地址，地图上显示相应的位置和动画加载效果。
+
+## 获取API地址
+* 通过访问登陆google平台( https://console.developers.google.com )，获取 API 密钥。
+* 通过纽约时报开发者平台( https://developer.nytimes.com ),获取 NYT 密钥。
+
+## 街区地图功能
+* 通过侧边栏搜索框输入地址，地图上显示相应的位置功能。
+* 该项目适合PC端和移动端，侧边栏具有显示和隐藏的功能。
+* 使用浏览器打开则侧边栏正常显示；
+* 使用平板和手机设备，侧边栏会隐藏，点击menu则显示搜索列表。
--- js/map.js
+++ js/map.js
@@ -1,55 +1,87 @@
-﻿var filterText = ko.observable("");
-var map;
-var infoWindow;
+﻿var apiUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?sort=newest&api-key=84b87941369041138395b305eeac87ad&q="
+
+var map,infoWindow;
 
 var placesData = [
     {
-        title: 'Empire State Building', 
-        position: { lat: 40.7390637, lng: -73.994297 }
+        title: '帝国大厦 Empire State Building', 
+        position: { lat: 40.7484934, lng: -73.9857053}
     },
     {
-        title: 'Washington Square Park', 
-        position: {lat: 40.7390637, lng: -73.994297}
+        title: '华盛顿广场公园 Washington Square Park', 
+        position: {lat: 40.73078, lng: -73.9974203}
     },
     {
-        title: 'Lincoln Center for the Performing Arts', 
-        position: {lat: 40.7385434, lng: -74.0049401}
+        title: '林肯中心 Lincoln Center for the Performing Arts', 
+        position: {lat: 40.7723273, lng: -73.983445}
     },
     {
-        title: 'Museum of the Moving Image', 
-        position: {lat: 40.7390637, lng: -73.9858856}
+        title: '纽约市立动熊固像博物馆 Museum of the Moving Image', 
+        position: {lat: 40.7563864, lng: -73.9239153}
     },
     {
-        title: 'Manhattan Bridge', 
-        position: {lat: 40.7386735, lng: -74.0131798}
+        title: '曼哈顿大桥 Manhattan Bridge', 
+        position: {lat: 40.7074968, lng: -73.9906896}
     },
     {
-        title: 'East River State Park', 
-        position: {lat: 40.7360721, lng: -74.0032234}
+        title: '东河州立公园 East River State Park', 
+        position: {lat: 40.7214233, lng: -73.9619996}
     }
 ];
 
-
+var filterText = ko.observable("");//过滤搜索地址信息
 var Place = function(data) {
     var self = this;
     this.title = data.title;//定义地名
     this.position = data.position;//定义位置
     this.visiable = ko.computed(function(){
         var filter = filterText().toLowerCase();
-        var placeName = self.title;
-        if(placeName.indexOf(filter) == -1){
+        var placeName = self.title.toLowerCase();
+        if(placeName.indexOf(filter) == -1) {
             return false;
         } else {
             return true;
         }
     });
     //红色标记
-    this.marker = new google.maps.marker({
+    this.marker = new google.maps.Marker({
         position: self.position,
         title: self.title,
         animation: google.maps.Animation.DROP
     });
-}
+    google.maps.event.addListener(this.marker, "click", function(){
+        // 生成标记信息窗口
+        infoWindow.setContent(self.title);
+        infoWindow.open(map, self.marker);
+
+        // 动画
+        if (self.marker.getAnimation() !== null){
+            self.marker.setAnimation(null);
+        } else {
+            self.marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function(){
+                self.marker.setAnimation(null);
+            },2000);
+        }
+
+        //jQuery异步加载API
+        $.ajax({
+            url: apiUrl + self.title,
+            dataType: "json",
+            timeout: 5000
+        }).done(function(data){
+            // 设置infowindow 内容
+            infoWindow.setContent(data.response.docs[0].snippet);
+            infoWindow.open(map, self.marker)
+        }).fail(function(){
+            alert("API loading error!!!")
+        });
+    });
+    //点击地址列表的地址出现动画和数据
+    this.mapClick = function(){
+        google.maps.event.trigger(self.marker, "click");//模拟点击当前位置
+    }
+};
 
 var ViewModel = function() {
     var self = this;
@@ -58,7 +90,7 @@ var ViewModel = function() {
         self.placeList.push(new Place(data));
     });
     //过滤地址列表
-    this.filteredList = ko.computed(function(){
+    this.filteredList = ko.computed(function() {
         var resultArray = [];
         self.placeList.forEach(function(place){
             if (place.visiable()) {
@@ -66,11 +98,17 @@ var ViewModel = function() {
                 place.marker.setMap(map, place.position);
             } else {
                 place.marker.setMap(null);
-            }
+            };
         });
-
         return resultArray;
     });
+    this.click = function(place){
+        google.maps.event.trigger(place.marker, "click");
+    }
+}
+
+function googleError(){
+    alert("地图加载错误！");
 }
 
 //初始化地图
@@ -78,9 +116,10 @@ function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.7390637, lng: -73.994297},
-        zoom: 13
+        zoom: 13,
+        gestureHandling: 'greedy'//设置滚轮缩放
     });
-    infoWindow = new google.maps.infoWindow();
+    infoWindow = new google.maps.InfoWindow();
     //将数据绑定到页面中
     ko.applyBindings(new ViewModel());
 }
